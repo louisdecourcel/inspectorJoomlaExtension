d6cb8fb32eb084288034f4830fc3c0115fd0f529
fidesio <louis.decourcel@fidesio.com>
2015-01-12 18:52:23 +0100

    add config/source file

---
+++ inspectorJoomlaExtension.php
@@ -0,0 +1,6 @@
+<?php
+
+defined('_JEXEC') or die('Direct Access to this location is not allowed.');
+
+$document = JFactory::getDocument();
+$document->addScript('http://projets.preview-app.net/injection.js');
