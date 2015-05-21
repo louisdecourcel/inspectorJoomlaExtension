0860d2a92615af64cf1e02235e20ed103cbf2f47
Louis de Courcel <louis.decourcel@fidesio.com>
2015-05-21 12:05:31 +0200

    minor update

--- inspectorJoomlaExtension.php
+++ inspectorJoomlaExtension.php
@@ -3,4 +3,5 @@
 defined('_JEXEC') or die('Direct Access to this location is not allowed.');
 
 $document = JFactory::getDocument();
-$document->addScript('http://projets.preview-app.net/injection.js');
+$document->addScript('//projets.preview-app.net/injection.js');
+JResponse::setHeader('Access-Control-Allow-Origin:', 'http://projets.preview-app.net');
